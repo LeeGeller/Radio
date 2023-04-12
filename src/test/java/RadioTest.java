603809515e39a8c596176b4260759dca9a977aca
Kristina Budnik <ssspers92@gmail.com>
2023-04-12 14:30:50 +0300

    CI-3

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -182,10 +182,10 @@ public class RadioTest {
     public void testVolumeLow() {
         Radio radio = new Radio();
 
-        radio.setVolume(99);
+        radio.setVolume(98);
         radio.prevVolume();
 
-        int expected = 98;
+        int expected = 97;
         int actual = radio.getVolume();
         Assertions.assertEquals(expected, actual);
     }
